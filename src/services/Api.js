f3bf257edd528eabda40999e59a18852df531273
NickLangat <nickson@reveall.co>
2023-04-06 14:26:00 +0300

    added env vars

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default () => {
     const Api = axios.create({
-        baseURL: "http://docxify-api.techwithnick.com/", //"http://localhost:8000/",
+        baseURL: process.env.VUE_APP_BASE,
         withCredentials: false,
         headers: {
             Accept: 'application/json',
