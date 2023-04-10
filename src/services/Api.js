e78fb7330ca0e1ecfdc27ac42501e3304be19cff
NickLangat <nickson@reveall.co>
2023-04-10 14:19:10 +0300

    upload documents working

--- src/services/Api.js
+++ src/services/Api.js
@@ -4,6 +4,7 @@ export default () => {
     const Api = axios.create({
         baseURL: process.env.VUE_APP_BASE,
         withCredentials: false,
+        
         headers: {
             Accept: 'application/json',
             'Content-Type': 'application/json',
